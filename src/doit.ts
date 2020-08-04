7de3c664c3d58cae050c4fc786e872f5d3d13940
Danny Krause <vanreece@gmail.com>
2020-08-04 15:54:51 -0700

    Can toggle and untoggle favorite with sorting
    Next: add CSS transitions for the mover

--- src/doit.ts
+++ src/doit.ts
@@ -1,15 +1,66 @@
+async function init() {
+    const selection = d3.select('#wrapper');
+    selection.selectAll('ol').data([0]).enter().append('ol');
+}
+
+function sortSources(a: datum, b: datum) {
+    return a.favorite && !b.favorite ? -1
+        : b.favorite && !a.favorite ? 1
+        : a.best && !b.best ? -1
+        : b.best && !a.best ? 1
+        : d3.ascending(a.name, b.name) || 0;
+}
+
+type datum = {
+    name: string,
+    best?: boolean,
+    favorite?: boolean,
+}
+
+const data: datum[] = [
+    {'name': 'traitor', best: true},
+    {'name': 'pianist', best: false},
+    {'name': 'nondefinitive'},
+    {'name': 'combinedly'},
+    {'name': 'predelivery'},
+    {'name': 'raker'},
+    {'name': 'preinsinuatingly'},
+    {'name': 'anconeous'},
+    {'name': 'capuchin'},
+    {'name': 'staio'},
+    {'name': 'preoccipital'},
+]
+
+async function toggleFavorite(index: number) {
+    if(data[index].favorite) {
+        data[index].favorite = false;
+    } else {
+        data[index].favorite = true;
+    }
+    doit();
+}
+
 async function doit() {
-    const selection = d3.select("#wrapper");
-    const div = selection.append("div");
-    const ol = div.append("ol");
-    ol
-        .append("li")
-        .html("Hi there");
-    ol
-        .append("li")
-        .html("Nice to meet you");
-  
-    console.log("hi");
+    const selection = d3.select('#wrapper');
+    const list = selection.selectAll('ol');
+
+
+    const listElements = list.selectAll('li').data(data);
+    listElements
+        // .text(function(d) {return `Existing: ${d.name}`})
+        .text(function(d) {return `${d.name}`})
+        .enter()
+
+    listElements.enter()
+        .append('li')
+        // .text(function(d) {return `New: ${d.name}`})
+        .text(function(d) {return `${d.name}`})
+
+    listElements.exit()
+        .remove()
+    
+    list.selectAll('li').sort(sortSources);
 }
   
+init();
 doit();
